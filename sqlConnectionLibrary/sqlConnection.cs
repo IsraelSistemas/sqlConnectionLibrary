94490d05a5eb4ccf08010b0895173f871ba65a49
IsraelSistemas19 <israel_sistemas@hotmail.com>
2019-12-01 15:57:47 -0600

    TestConnection method was added

---
+++ sqlConnectionLibrary/sqlConnection.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace sqlConnectionLibrary
+{
+    public class sqlConnection
+    {
+        SqlConnection con = new SqlConnection("Data Source=LAPTOP-SU6PUUVF\\SQLEXPRESS;Initial Catalog=demo;Integrated Security=True");
+
+        public bool TestConnection()
+        {
+            bool success = false;
+
+            try
+            {
+                con.Open();
+                success = true;
+                Console.WriteLine("Connection OK");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Connection failed: \n" + ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+
+            return success;
+        }
+    }
+}
